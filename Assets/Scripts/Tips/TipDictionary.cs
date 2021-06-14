c9d8c212d8cf688dd29beb64fe91d1cd6d2d8c48
kori521 <71168208+kori521@users.noreply.github.com>
2021-06-14 08:02:18 +0200

    tipDev

--- Assets/Scripts/Tips/TipDictionary.cs
+++ Assets/Scripts/Tips/TipDictionary.cs
@@ -5,23 +5,26 @@ using System.IO;
 
 public class TipDictionary : BehaviourButton
 {
+    
     [System.Serializable]
-    public struct TipData{
+    public struct RolledTips
+    {
+        public string name;
         public string data;
-        public string LastDate;
+        public string lastdate;
     }
-    List<string> dates = new List<string>();
     List<string> datas = new List<string>();
+    List<string> name = new List<string>();
+    List<string> maindata = new List<string>();
     IDictionary<int, string> Tips = new Dictionary<int, string>();
     public bool first_run = true;
-    public int i = 0;
+    public int c = 0;
     public void TipLoad()
     {
         List<string> _tips = new List<string>();
-        StreamReader sr = new StreamReader("tips.txt");
+        StreamReader sr = new StreamReader(@"Assets\Scripts\Tips\tips.txt");
         try
-        {
-            
+        {         
             string sor;
             do
             {
@@ -36,14 +39,22 @@ public class TipDictionary : BehaviourButton
             Debug.LogError($"Error loading in the tips!");
             throw;
         }
+        string[] cut = new string[2];
         for (int i = 0; i < _tips.Count; i++)
         {
             Tips.Add(i,_tips[i]);
         }
-		//for (int i = 0; i < Tips.Count; i++)
-		//{
-		//	print(Tips[i]);
-		//}
+        for (int i = 0; i < _tips.Count; i++)
+        {
+            print(_tips[i]);
+            cut = _tips[i].Split('-');
+            name.Add(cut[0]);
+            maindata.Add(cut[1]);
+        }
+        //for (int i = 0; i < Tips.Count; i++)
+        //{
+        //	print(Tips[i]);
+        //}
     }  
 
     public string FindTip(int _key)
@@ -61,21 +72,36 @@ public class TipDictionary : BehaviourButton
         }    
     }
 
-
-    public string GetRandomTip()
-    {       
-        if(Tips.Count < 1) { Debug.LogWarning($"Dictionary is empty: {Tips}"); return null; }      
+    public RolledTips GetRandomTip()
+    {
+        RolledTips rolledtip;
+        int a = 0;
         int _key = Random.Range(0, Tips.Count);
-        print(Tips[_key]);
-        string load = Tips[_key]; 
-        TipData lastTip;
-        lastTip.data = load;           
-        lastTip.LastDate = System.DateTime.Now.ToString();
-        datas.Add(lastTip.data);
-        dates.Add(lastTip.LastDate);
-        print(dates[i] + " " + datas[i]);
-        i++;
-        return Tips[_key];
+        //print(mainlist.data[_key]);            
+        rolledtip.data = maindata[_key];
+        rolledtip.name = name[_key];
+        rolledtip.lastdate = System.DateTime.Now.ToString();
+        datas.Add(_key + " " + rolledtip.name + " " + rolledtip.data + " " + rolledtip.lastdate);
+        //print(datas[a]);
+        a++;
+        return rolledtip;
+    }
+
+    public void SavedTips()
+    {
+        StreamWriter sw = new StreamWriter(@"Assets\Scripts\Tips\SavedTips.txt", append: true); //,append: true)
+        try
+        {
+            print(datas[c]);
+            sw.WriteLine(datas[c]);
+            c++;
+            sw.Close();
+        }
+        catch (IOException)
+        {
+            Debug.LogError($"Error saving the tips!");
+            throw;
+        }
     }
 
     protected override void OnTouch()
@@ -86,6 +112,6 @@ public class TipDictionary : BehaviourButton
             first_run = false;
         }
         GetRandomTip();
-        
+        SavedTips();
     }
 }
