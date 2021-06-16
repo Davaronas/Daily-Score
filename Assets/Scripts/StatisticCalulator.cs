31545921b9dd6cfc5b674cc27704370fb816bfa0
Szabo Krisztian <43440140+Wulfy99@users.noreply.github.com>
2021-06-16 14:39:44 +0200

    Update StatisticCalulator.cs

--- Assets/Scripts/StatisticCalulator.cs
+++ Assets/Scripts/StatisticCalulator.cs
@@ -9,12 +9,11 @@ public class StatisticCalculator : MonoBehaviour
 {
      public GoalManager goalManager = null;
      public GoalData[] goalDatas = null;
-    // Start is called before the first frame update
     void Start()
     {
         goalManager = FindObjectOfType<GoalManager>();
         goalDatas = goalManager.GetGoals();
-        DateTime Today = DateTime.Now;
+        DateTime Today = DateTime.Today;
         DateTime LastWeek = Today.AddDays(-7);
         DateTime Test;
         Console.WriteLine(Today.ToString("{0}")); //Debug
@@ -23,23 +22,24 @@ public class StatisticCalculator : MonoBehaviour
             goalDatas[i].GetLastModificationTime();
             int change = goalDatas[i].lastChange.amount;
         }
-        int max = 0;//Napi max
+        int max = 0;//A MAX ami mindenkori lesz.
 
-        int avarage; //0
-        int kkavp=0; //0
-        int kkavperm = kkavp/7; //0
-        for (int i=0; i<7;i++)
-        {
-             int kkmax = 0; //0
-            if(0 >max)
+        while () //Napi adatokig kell mennie 
+        { 
+            int avarage; //átlag
+            int kkavp = 0; //0
+            int kkavperm = kkavp / 7; //0
+            for (int i = 0; i < 7; i++)
             {
-                max = 0; //0
+                int kkmax = 0; //Ez folyton változó napi adat.
+                if (0 > max)
+                {
+                    max = kkmax; //0
+                }
+                int kkav = 0; //0
             }
-            int kkav = 0; //0
-        }
-        kkavp = kkav;
 
-        
+        }
     }
 
     
